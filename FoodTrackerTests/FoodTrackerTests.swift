6a34f019fed341a8f431b56020afc2ff3caee39f
BowmanZone <524009730@qq.com>
2016-09-05 11:07:26 +0800

    新建模型、模型的测试用例

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -9,28 +9,25 @@
 import XCTest
 @testable import FoodTracker
 
+// Tests to confirm that the Meal initializer returns when no name or a negative rating is provided.
 class FoodTrackerTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    //MARK: FoodTracker Tests
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+    func testMealInitialization() {
+        //通过测试几组数据，和你自己设计的是否是一样的效果
+        //success
+        let potentialItem = Meal.init(name: "Newest meal", photo: nil, rating: 5)
+        XCTAssertNotNil(potentialItem)
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
+        //failed
+        //no name
+        let noName = Meal.init(name: "", photo: nil, rating: 0)
+        XCTAssertNil(noName)
+        //bad rating
+        let badRating = Meal.init(name: "Really bad rating", photo: nil, rating: -1)
+        XCTAssertNil(badRating, "Negative ratings are invalid, be positive")
+        
     }
     
 }
